bf5bac0deea439fd533fa9bbab0afe9045461bd6
TalpaCristian <98746367+TalpaCristian@users.noreply.github.com>
2022-03-11 13:18:03 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Counter from "./Counter";
+
+const App =() => {
+    return (
+        <div className='app'>
+            <Counter/>
+        </div>
+    );
+};
+
+export defoult App
