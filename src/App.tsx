8c47cbee2ebfc3897bb09e8133cea060579855f8
TalpaCristian <cuzea222@gmail.com>
2022-04-11 03:20:27 +0300

    import FormModel in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,17 @@ import {Layout} from "antd";
 import {Footer} from "antd/es/layout/layout";
 import ContentIn from "./Componente/Layout/ContentIn";
 import HeaderIn from "./Componente/Layout/HeaderIn";
+import { FormModel } from './Componente/Layout/FormModel';
 
 function App() {
   return (
       <Layout>
 
           <HeaderIn/>
-          <div style={{ padding: '150px 50px 50px 50px'
+          <div style={{ padding: '80px 10px 10px 10px'
+          }}/>
+          <FormModel/>
+          <div style={{ padding: '80px 50px 50px 50px'
           }}/>
           <ContentIn />
         <Footer style={{ textAlign: 'center' }}>Talpa Cristian</Footer>
